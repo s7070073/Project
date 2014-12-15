50730c8342bb622c8f21c575f68f0eb31d18d14e
Champ Phattarapon <buntoo2538@gmail.com>
2014-12-15 19:02:18 +0700

    add buttom
    add buttom

--- histogram.py
+++ histogram.py
@@ -14,18 +14,22 @@ def callback():
     print 'Hello World.'
 
 #input box
-input_box_label = Label(app, text='Champ')
+input_box_label = Label(app, text='Data Input')
 input_box_label.place(x=20, y=5)
-input_box = Entry(app, width=30)
+input_box = Entry(app, width=100)
 input_box.place(x=20, y=30)
 
+
+#process_button
+process_button = Button(app, text='Process',height=3, width=10)
+process_button.place(x=670, y=30)
 
 
 #len_data
 len_data_label = Label(app, text='len data')
 len_data_label.place(x=170, y=500)
 len_data = Listbox(app, height=1, width=10)
-len_data.insert(1, 'champ')
+len_data.insert(1, 321)
 len_data.place(x=250, y=500)
 
 #max_data
@@ -75,7 +79,7 @@ range_data.place(x=630, y=550)
 
 
 #histro
-canvas_area = Canvas(app, width=750, height=350, background='red')
+canvas_area = Canvas(app, width=750, height=350, background='green')
 canvas_area.place(x=20, y=100)
 
 canvas_area.create_rectangle(10, 10, 30, 30)
